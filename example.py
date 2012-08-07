1d0576be4868d02c7de2f889bad1c14bd47ae3d7
Matt Robenolt <matt@ydekproductions.com>
2012-08-07 06:48:12 -0700

    Support for loading credentials from CSV, fixes #2

--- example.py
+++ example.py
@@ -3,6 +3,7 @@ import ec2
 ec2.credentials.ACCESS_KEY_ID = 'xxx'
 ec2.credentials.SECRET_ACCESS_KEY = 'xxx'
 ec2.credentials.REGION_NAME = 'us-west-2'
+# ec2.credentials.from_file('credentials.csv')
 
 print ec2.instances.all()
 for i in ec2.instances.filter(state__iexact='rUnning', name__endswith='01', name__startswith='production'):
